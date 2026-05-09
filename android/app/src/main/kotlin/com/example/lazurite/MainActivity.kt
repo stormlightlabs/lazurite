95246e4c5ee11098be5f7fed0bc574ef5426f195
Owais Jamil <desertthunder.dev@gmail.com>
2026-05-09 09:07:10 -0500

    fix: wake-lock guard

--- android/app/src/main/kotlin/com/example/lazurite/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/lazurite/MainActivity.kt
@@ -1,5 +1,25 @@
 package org.stormlightlabs.lazurite
 
+import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onPause() {
+        clearLockInterferingWindowFlags()
+        super.onPause()
+    }
+
+    override fun onStop() {
+        clearLockInterferingWindowFlags()
+        super.onStop()
+    }
+
+    private fun clearLockInterferingWindowFlags() {
+        window.clearFlags(
+            WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON or
+                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
+                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON or
+                WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
+        )
+    }
+}
